925db79369d3ca2f65a864a3e2d71da8bd4b3e1e
Evgeny Losev <evgeny@diesellabs.com>
2022-01-23 16:20:42 -0500

    HomeWorks: collections, futures, collections again, zio intro

--- src/main/scala/collections/task_collections.scala
+++ src/main/scala/collections/task_collections.scala
@@ -1,6 +1,13 @@
 package collections
 
-object task_collections {
+import scala.util.{Failure, Success, Try}
+
+object task_collections extends App {
+
+  val text = "Hello. I am 9 years old"
+  val input =  ("""\d+""".r findAllIn text).toList
+  println(s"input = $input")
+  println(text.substring(0, text.indexOf(input.head)) + input.head + text.substring(text.indexOf(input.head)+ input.head.length, text.length) )
 
   def isASCIIString(str: String): Boolean = str.matches("[A-Za-z]+")
 
@@ -16,7 +23,12 @@ object task_collections {
    *
    * **/
   def capitalizeIgnoringASCII(text: List[String]): List[String] = {
-    List.empty
+    (List(text.head) ::: text.drop(1).map { el =>
+      isASCIIString(el) match {
+        case true => el.toUpperCase
+        case false => el.toLowerCase
+      }
+    })
   }
 
   /**
@@ -29,7 +41,100 @@ object task_collections {
    * HINT: Для всех возможных комбинаций чисел стоит использовать Map
    * **/
   def numbersToNumericString(text: String): String = {
-    ""
+
+    val input =  ("""\d+""".r findAllIn text).toList
+    println(s"input = $input")
+    import java.text.DecimalFormat
+    val tensNames = Array("", " ten", " twenty", " thirty", " forty", " fifty", " sixty", " seventy", " eighty", " ninety")
+
+    val numNames = Array("", " one", " two", " three", " four", " five", " six", " seven", " eight", " nine", " ten", " eleven", " twelve", " thirteen", " fourteen", " fifteen", " sixteen", " seventeen", " eighteen", " nineteen")
+
+    def convertLessThanOneThousand(number: Int): String = {
+      var soFar = ""
+      var num = number
+      if (number % 100 < 20) {
+        soFar = numNames(number % 100)
+        num = number / 100
+      }
+      else {
+        soFar = numNames(number % 10)
+        num = number / 10
+        soFar = tensNames(number % 10) + soFar
+        num = number / 10
+      }
+      if (num == 0) {
+        soFar
+      } else numNames(num) + " hundred" + soFar
+    }
+
+
+    def convert(number: Long): String = { // 0 to 999 999 999 999
+      if (number == 0) {
+        "zero"
+      } else {
+        var snumber = number.toString
+        // pad with "0"
+        val mask = "000000000000"
+        val df = new DecimalFormat(mask)
+        snumber = df.format(number)
+        // XXXnnnnnnnnn
+        val billions = snumber.substring(0, 3).toInt
+        // nnnXXXnnnnnn
+        val millions = snumber.substring(3, 6).toInt
+        // nnnnnnXXXnnn
+        val hundredThousands = snumber.substring(6, 9).toInt
+        // nnnnnnnnnXXX
+        val thousands = snumber.substring(9, 12).toInt
+        var tradBillions = ""
+        billions match {
+          case 0 =>
+            tradBillions = ""
+
+          case 1 =>
+            tradBillions = convertLessThanOneThousand(billions) + " billion "
+
+          case _ =>
+            tradBillions = convertLessThanOneThousand(billions) + " billion "
+        }
+        var result = tradBillions
+        var tradMillions = ""
+        millions match {
+          case 0 =>
+            tradMillions = ""
+
+          case 1 =>
+            tradMillions = convertLessThanOneThousand(millions) + " million "
+
+          case _ =>
+            tradMillions = convertLessThanOneThousand(millions) + " million "
+        }
+        result = result + tradMillions
+        var tradHundredThousands = ""
+        hundredThousands match {
+          case 0 =>
+            tradHundredThousands = ""
+
+          case 1 =>
+            tradHundredThousands = "one thousand "
+
+          case _ =>
+            tradHundredThousands = convertLessThanOneThousand(hundredThousands) + " thousand "
+        }
+        result = result + tradHundredThousands
+        var tradThousand = ""
+        tradThousand = convertLessThanOneThousand(thousands)
+        result = result + tradThousand
+        // remove extra spaces!
+        result.replaceAll("^\\s+", "").replaceAll("\\b\\s{2,}\\b", " ")
+      }
+    }
+
+    Try(convert(input.head.toLong)) match {
+      case Failure(_) => text
+      case Success(value) =>
+        //value
+        text.substring(0, text.indexOf(input.head)) + value + text.substring(text.indexOf(input.head)+ input.head.length, text.length)
+    }
   }
 
   /**
@@ -47,7 +152,7 @@ object task_collections {
    * Реализуйте метод который примет две коллекции (два источника) и вернёт объединенный список уникальный значений
    **/
   def intersectionAuto(dealerOne: Iterable[Auto], dealerTwo: Iterable[Auto]): Iterable[Auto] = {
-    Iterable.empty
+    dealerOne.toSet intersect dealerTwo.toSet
   }
 
   /**
@@ -56,6 +161,6 @@ object task_collections {
    * и вернёт уникальный список машин обслуживающихся в первом дилерском центре и не обслуживающимся во втором
    **/
   def filterAllLeftDealerAutoWithoutRight(dealerOne: Iterable[Auto], dealerTwo: Iterable[Auto]): Iterable[Auto] = {
-    Iterable.empty
+    dealerOne.toSet diff dealerTwo.toSet
   }
 }
--- src/main/scala/module1/03-introducing_fp_concepts.scala
+++ src/main/scala/module1/03-introducing_fp_concepts.scala
@@ -1,9 +1,11 @@
 package module1
 
+
 import java.util.UUID
 import scala.annotation.tailrec
 import java.time.Instant
 import scala.language.postfixOps
+import scala.runtime.Nothing$
 
 
 
@@ -196,134 +198,227 @@ object hof{
    * Реализовать тип Option, который будет указывать на присутствие либо отсутсвие результата
    */
 
-   sealed trait Option[+T]{
-       def isEmpty: Boolean = this match {
-           case Option.Some(v) => false
-           case Option.None => true
-       }
-
-       def get: T = this match {
-           case Option.Some(v) => v
-           case Option.None => throw new Exception("Get on empty option")
-       }
-
-       def map[B](f: T => B): Option[B] = this match {
-           case Option.Some(v) => Option.Some(f(v))
-           case Option.None => Option.None
-       }
-
-       def flatMap[B](f: T => Option[B]): Option[B] = this match {
-           case Option.Some(v) => f(v)
-           case Option.None => Option.None
-       }
-
-       def printIfAny: Unit = this match {
-          case Option.Some(v) => println(v)
-          case Option.None =>
-        }
-   }
-
-   object Option{
-        case class Some[T](v: T) extends Option[T]
-        case object None extends Option[Nothing]
-
-        def apply[T](v: T): Option[T] = ???
-
-
-   }
-
-
-
-
-
-  /**
-   *
-   * Реализовать метод printIfAny, который будет печатать значение, если оно есть
-   */
-
-
-  /**
-   *
-   * Реализовать метод zip, который будет создавать Option от пары значений из 2-х Option
-   */
-
-
-  /**
-   *
-   * Реализовать метод filter, который будет возвращать не пустой Option
-   * в случае если исходный не пуст и предикат от значения = true
-   */
-
- }
-
- object list {
-   /**
-    *
-    * Реализовать односвязанный иммутабельный список List
-    * Список имеет два случая:
-    * Nil - пустой список
-    * Cons - непустой, содердит первый элемент (голову) и хвост (оставшийся список)
-    */
-
-    sealed trait List[+T]{
-
-      def ::[A >: T](elem: A): List[A] = new :: (elem, this)
+  sealed trait Option[+T] {
+    def isEmpty: Boolean = this match {
+      case Option.Some(v) => false
+      case Option.None => true
     }
 
-    case class ::[A](head: A, tail: List[A]) extends List[A]
-    case object Nil extends List[Nothing]
-
-    object List {
-      def apply[T](v: T*): List[T] = if (v.isEmpty) Nil else new ::(v.head, apply(v.tail: _*))
+    def get: T = this match {
+      case Option.Some(v) => v
+      case Option.None => throw new Exception("Get on empty option")
     }
-    
+
+    def map[B](f: T => B): Option[B] = this match {
+      case Option.Some(v) => Option.Some(f(v))
+      case Option.None => Option.None
+    }
+
+    def flatMap[B](f: T => Option[B]): Option[B] = this match {
+      case Option.Some(v) => f(v)
+      case Option.None => Option.None
+    }
+
+    def printIfAny: Unit = this match {
+      case Option.Some(v) => println(v)
+      case Option.None =>
+    }
+
+
+    object Option {
+      case class Some[T](v: T) extends Option[T]
+
+      case object None extends Option[Nothing]
+
+      def apply[T](v: T): Option[T] = ???
+
+
+    }
+
 
 
     /**
-     * Метод cons, добавляет элемент в голову списка, для этого метода можно воспользоваться названием `::`
      *
+     * Реализовать метод printIfAny, который будет печатать значение, если оно есть
      */
 
-    /**
-      * Метод mkString возвращает строковое представление списка, с учетом переданного разделителя
-      *
-      */
+    def printIfAny[A](v: Option[A]) = {
+      v match {
+        case Option.Some(v) => println(v)
+        case Option.None => throw new Exception("There is nothing to print")
+      }
+    }
 
     /**
-      * Конструктор, позволяющий создать список из N - го числа аргументов
-      * Для этого можно воспользоваться *
-      * 
-      * Например вот этот метод принимает некую последовательность аргументов с типом Int и выводит их на печать
-      * def printArgs(args: Int*) = args.foreach(println(_))
-      */
+     *
+     * Реализовать метод zip, который будет создавать Option от пары значений из 2-х Option
+     */
+
+
+    final def zip[A, B](other: Option[B]) = {
+      if (!other.isEmpty && !this.isEmpty) Some((other.get, this.get))
+      else if (other.isEmpty && !this.isEmpty) Some((null, this.get))
+      else if (!other.isEmpty && this.isEmpty) Some((other.get, null))
+      else None
+    }
 
     /**
-      *
-      * Реализовать метод reverse который позволит заменить порядок элементов в списке на противоположный
-      */
+     *
+     * Реализовать метод filter, который будет возвращать не пустой Option
+     * в случае если исходный не пуст и предикат от значения = true
+     */
 
+    def filter(f: T => Boolean) = if (f(this.get)) this else None
+  }
+
+  object list {
     /**
-      *
-      * Реализовать метод map для списка который будет применять некую ф-цию к элементам данного списка
-      */
+     *
+     * Реализовать односвязанный иммутабельный список List
+     * Список имеет два случая:
+     * Nil - пустой список
+     * Cons - непустой, содердит первый элемент (голову) и хвост (оставшийся список)
+     */
+
+    import List._
+    sealed trait List[+A]{
+      def ::[AA >: A](head: AA): List[AA] = Cons(head, this)
+
+      def head(): A
+      def tail(): List[A]
+      def mkString: String = mkString(", ")
+
+      def mkString(sep: String): String = {
+        def go(l: List[A], acc: StringBuilder): StringBuilder = {
+          l match {
+            case List.Nil => acc
+            case h :: Nil => acc.append(s"$h")
+            case h :: t => go(t, acc.append(s"$h$sep"))
+          }
+        }
+        go(this, new StringBuilder()).toString()
+      }
+
+      def reverse: List[A] = {
+        def go(input: List[A], acc: List[A] = List[A]()): List[A] = {
+          input match {
+            case List.Nil => acc
+            case List.::(head, tail) => go(tail, head :: acc)
+          }
+        }
+        go(this)
+      }
+
+      def funcElem[B](f: A => B): List[B] = {
+        def go(xs: List[A], acc: List[B] = List[B]()): List[B] = {
+          xs match {
+            case List.Nil => acc
+            case List.::(head, tail) => go(tail, f(head) :: acc)
+          }
+        }
+        go(this)
+      }
+
+      def map[B](f: (A) => B): List[B] = {
+        this match {
+          case Cons(xh: A, Nil) => Cons(f(xh), Nil)
+          case Cons(xh: A, xs: List[A]) => Cons(f(xh), xs.map(f))
+          case a: List[A] => Cons(f(a.head()), a.tail.map(f))
+        }
+      }
+      def   fail(m: String) = throw new NoSuchElementException(m)
+    }
 
 
-    /**
-      *
-      * Реализовать метод filter для списка который будет фильтровать список по некому условию
-      */
+    object List{
+      case object Nil extends List[Nothing] {
+        override def head: Nothing = fail("An empty list.")
+        override def tail: List[Nothing] = fail("An empty list.")
+      }
+      case class ::[A](head: A, tail: List[A]) extends List[A]
+      val Cons = ::
 
-    /**
-      *
-      * Написать функцию incList котрая будет принимать список Int и возвращать список,
-      * где каждый элемент будет увеличен на 1
-      */
+      def apply[T](arg: T*): List[T] = {
+        var l: List[T] = List.Nil
+        arg.foreach(el => l = el :: l)
+        l
+      }
+
+      def incList(xs: List[Int]): List[Int] = {
+        def go(xs: List[Int], acc: List[Int] = List[Int]()): List[Int] = {
+          xs match {
+            case List.Nil => acc
+            case List.::(head, tail) => go(tail, head + 1 :: acc)
+          }
+        }
+        go(xs)
+      }
+
+      def shoutString(xs: List[String]): List[String] = {
+        def go(xs: List[String], acc: List[String] = List[String]()): List[String] = {
+          xs match {
+            case Nil => acc
+            case ::(head, tail) => go(tail, "!" + head :: acc )
+          }
+        }
+        go(xs)
+      }
+    }
 
 
-    /**
-      *
-      * Написать функцию shoutString котрая будет принимать список String и возвращать список,
-      * где к каждому элементу будет добавлен префикс в виде '!'
-      */
 
- }
+    val list = 1 :: List.Nil
+
+      /**
+       * Метод cons, добавляет элемент в голову списка, для этого метода можно воспользоваться названием `::`
+       *
+       */
+
+
+
+      /**
+       * Метод mkString возвращает строковое представление списка, с учетом переданного разделителя
+       *
+       */
+
+
+
+      /**
+       * Конструктор, позволяющий создать список из N - го числа аргументов
+       * Для этого можно воспользоваться *
+       *
+       * Например вот этот метод принимает некую последовательность аргументов с типом Int и выводит их на печать
+       * def printArgs(args: Int*) = args.foreach(println(_))
+       */
+
+      /**
+       *
+       * Реализовать метод reverse который позволит заменить порядок элементов в списке на противоположный
+       */
+
+      /**
+       *
+       * Реализовать метод map для списка который будет применять некую ф-цию к элементам данного списка
+       */
+
+
+      /**
+       *
+       * Реализовать метод filter для списка который будет фильтровать список по некому условию
+       */
+
+      /**
+       *
+       * Написать функцию incList котрая будет принимать список Int и возвращать список,
+       * где каждый элемент будет увеличен на 1
+       */
+
+
+      /**
+       *
+       * Написать функцию shoutString котрая будет принимать список String и возвращать список,
+       * где к каждому элементу будет добавлен префикс в виде '!'
+       */
+    }
+
+}
